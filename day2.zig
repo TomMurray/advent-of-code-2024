68d377569fe329df14b3356a762da274fdb65f98
Tom Murray <tom.paul.murray@gmail.com>
2024-12-03 08:07:35 +0000

    Remove debug logging

--- day2.zig
+++ day2.zig
@@ -19,23 +19,18 @@ fn check_report(comptime IterT: type, _it: IterT) !bool {
     var it = _it;
     const DataType = i32;
     var state = CheckState{};
-    std.log.debug("Started new line", .{});
     return while (it.next()) |entry| {
         const val = try std.fmt.parseInt(DataType, entry, 10);
-        std.log.debug("  Current value is {d}", .{val});
         if (state.val) |prev_val| {
             const diff = val - prev_val;
             const dir = if (diff >= 0) Direction.Asc else Direction.Desc;
-            std.log.debug("diff={d}, dir is {s}", .{ diff, @tagName(dir) });
             if (state.dir) |prev_dir| {
                 if (dir != prev_dir) {
-                    std.log.debug("Direction didn't match, aborting", .{});
                     break false;
                 }
             }
             const abs_diff = @abs(diff);
             if (abs_diff < params.min_step or abs_diff > params.max_step) {
-                std.log.debug("Absolute difference was outside allowed range: {d}", .{abs_diff});
                 break false;
             }
             state.dir = dir;
