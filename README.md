01c0fa8b79e7d6758c81f453925cff037c56910c
Tom Murray <tom.paul.murray@gmail.com>
2024-12-03 09:35:43 +0000

    Attempts to fully solve part 2 of day 2

--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 
 This year I'm trying out zig for advent of code. I always find it's a nice way to try and get some practical use of a new language. Please excuse any poor zig practice as I get used to it!
 
-To get the solutions for a day just use `zig build-exe` e.g.:
+Solutions for a day are built with `zig build-exe day<N>.zig` e.g.:
 ```bash
 zig build-exe day1.zig
+```
+
+Binaries usually are run with a single argument, the path to a file containing the input data for the day e.g.:
+```
 ./day1 inputs/day1.txt
 ```
+However, some solutions use command line parameters for part 1 & 2 solutions.
--- day2.zig
+++ day2.zig
@@ -18,7 +18,7 @@ fn CheckState(comptime IterT: type) type {
     };
 }
 
-fn check_report(comptime StateT: type, state_: StateT) !bool {
+fn check_report(comptime StateT: type, state_: StateT, remaining_failures: u32) !bool {
     var state = state_;
     const DataType = i32;
     var prev_state: ?StateT = null;
@@ -27,30 +27,48 @@ fn check_report(comptime StateT: type, state_: StateT) !bool {
         if (state.val) |prev_val| {
             const diff = val - prev_val;
             const dir = if (diff >= 0) Direction.Asc else Direction.Desc;
-            if (state.dir) |prev_dir| {
-                if (dir != prev_dir) {
-                    break false;
-                }
-            }
             const abs_diff = @abs(diff);
-            if (abs_diff < params.min_step or abs_diff > params.max_step) {
+            if (abs_diff < params.min_step or abs_diff > params.max_step or
+                if (state.dir) |prev_dir| prev_dir != dir else false)
+            {
+                if (remaining_failures > 0) {
+                    if (prev_state) |ps|
+                        if (try check_report(StateT, ps, remaining_failures - 1))
+                            break true;
+                    if (try check_report(StateT, state, remaining_failures - 1))
+                        break true;
+                }
                 break false;
+            }
+            if (prev_state) |*ps| {
+                ps.*.dir = state.dir;
             }
             state.dir = dir;
         }
+        if (prev_state == null) {
+            prev_state = StateT{ .it = state.it };
+        }
+        if (prev_state) |*ps| {
+            ps.*.val = state.val;
+        }
         state.val = val;
-        prev_state = state;
     } else true;
 }
+
+const Day2Error = error{
+    NotEnoughArgs,
+};
 
 pub const std_options: std.Options = .{ .log_level = .info };
 
 pub fn main() !void {
     const argv = std.os.argv;
-    if (argv.len != 2) {
-        std.log.err("2 arguments were required but only passed {d}", .{argv.len});
+    if (argv.len != 3) {
+        std.log.err("3 arguments were required but only passed {d}.\n  ./day2 <path to input> <max report entries to ignore (0 for part 1, 2 for part 2)>", .{argv.len});
+        return Day2Error.NotEnoughArgs;
     }
     const file_path = std.mem.span(argv[1]);
+    const max_failures = try std.fmt.parseInt(u32, std.mem.span(argv[2]), 10);
     const file = try std.fs.cwd().openFile(file_path, .{});
     var buf: [1024]u8 = undefined;
     var safe_count: u32 = 0;
@@ -58,7 +76,7 @@ pub fn main() !void {
     while (try file.reader().readUntilDelimiterOrEof(&buf, '\n')) |line| : (total_count += 1) {
         const entries = std.mem.splitScalar(u8, line, ' ');
         const init_state = CheckState(@TypeOf(entries)){ .it = entries };
-        safe_count += if (try check_report(@TypeOf(init_state), init_state)) 1 else 0;
+        safe_count += if (try check_report(@TypeOf(init_state), init_state, max_failures)) 1 else 0;
     }
     std.log.info("Total safe reports: {d}/{d}", .{ safe_count, total_count });
 }
